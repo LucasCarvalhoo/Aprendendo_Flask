103c8e2e4e27ab9ac60b34ed139c7193d05ead71
LucasCarvalhoo <lucas.carvalho7933@gmail.com>
2023-07-04 17:13:13 -0300

    capturando valores de variaveis

--- secao1/app.py
+++ secao1/app.py
@@ -7,7 +7,15 @@ app = Flask(__name__)
 # agora é criado uma rota
 @app.route('/')
 def principal():
-    return render_template("index.html")
+    fruta1 = "morango"
+    fruta2 = "uva"
+    fruta3 = "maçã"
+    fruta4 = "laranja"
+    return render_template("index.html", 
+                            fruta1=fruta1,
+                            fruta2=fruta2,
+                            fruta3=fruta3,
+                            fruta4=fruta4)
 
 @app.route('/sobre')
 def sobre():
