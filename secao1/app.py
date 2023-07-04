a79bd89aa7acc3bba38c5ec439479a2647c909be
LucasCarvalhoo <lucas.carvalho7933@gmail.com>
2023-07-04 18:50:04 -0300

    Adicionando dados em lista

--- secao1/app.py
+++ secao1/app.py
@@ -1,15 +1,18 @@
 # primeiro fazer a importação do flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # apos nomear a apclicação é definido o parametro da aplicação
 app = Flask(__name__)
-
+frutas = []
 # agora é criado uma rota
-@app.route('/')
+@app.route('/', methods=["GET", "POST", "DELETE"])
 def principal():
-    notas = {"Lucas": 9.0, "Lorena": 8.0, "Fatima": 10.0, "leoncio": 10.0} 
-    return render_template("index.html", notas=notas)
+    if request.method == "POST":
+        if request.form.get("fruta"):
+            frutas.append(request.form.get("fruta"))
+    return render_template("index.html", frutas=frutas)
 
 @app.route('/sobre')
 def sobre():
-    return render_template("sobre.html")
+    notas = {"Lucas": 9.0, "Lorena": 8.0, "Fatima": 10.0, "leoncio": 10.0} 
+    return render_template("sobre.html", notas=notas)
